2d2b9c65d5f18109059914eee133fb3627137423
Gidor <60816479+xGidor@users.noreply.github.com>
2023-11-05 14:56:43 +0100

    feat: implement string value for vm

--- src/vm/values.h
+++ src/vm/values.h
@@ -6,6 +6,7 @@
 */
 enum class ScriptyValueType {
     NUMBER,
+    STRING,
 };
 
 /**
@@ -15,6 +16,7 @@ struct ScriptyValue {
     ScriptyValueType type;
     union {
         double number;
+        std::string text;
     };
 };
 
@@ -28,9 +30,18 @@ inline ScriptyValue NUMBER(double value) {
     return result;
 }
 
+inline ScriptyValue STRING(std::string value) {
+    ScriptyValue result;
+    result.type = ScriptyValueType::STRING;
+    result.text = value;
+    return result;
+}
+
 /**
  * Accessors
 */
-#define AS_NUMBER(ScriptyValue) ((double)(ScriptyValue).number)
+#define AS_NUMBER(ScriptyValue) ((double)(ScriptyValue).number) // Number accessor
+#define AS_STRING(ScriptyValue) ((std::string)(ScriptyValue).text) // String accessor
+
 
 #endif
