fee1c453fbab0f4761b4ef44923668e3f242fe59
Gidor <60816479+xGidor@users.noreply.github.com>
2023-11-05 13:09:20 +0100

    feat: add opcodes halt & const

---
+++ src/vm/OpCode.h
@@ -0,0 +1,20 @@
+/**
+ * Instruction set for Scripty VM
+*/
+
+#ifndef OpCode_h
+#define OpCode_h
+
+/**
+ * Pushes a const onto the stack
+*/
+#define OP_CONST 0x00
+
+
+/**
+ * stops the program
+*/
+#define OP_HALT 0xff
+
+
+#endif
