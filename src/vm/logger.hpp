36ccbd1c49857b3357cc7487f5a049077b53fb7b
Gidor <60816479+xGidor@users.noreply.github.com>
2023-11-09 10:11:21 +0100

    feat(vm-logger): add WarningLogMessage

--- src/vm/logger.hpp
+++ src/vm/logger.hpp
@@ -10,10 +10,17 @@ public:
         fprintf(stderr, "Fatal error: &s\n", str().c_str());
         exit(EXIT_FAILURE);
     }
+};
 
+class WarningLogMessage : public std::basic_ostringstream<char> {
+public:
+    ~WarningLogMessage() {
+        fprintf(stderr, "Warning: &s\n", str().c_str());    
+    }
 };
 
 #define DIE ErrorLogMessage()
+#define WARN WarningLogMessage()
 
 #define log(value) std::cout << #value << " = " << (value) << "\n"; 
 
